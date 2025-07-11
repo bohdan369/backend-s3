36dc548b2977cf89c55c1a18c7453ca9473d28bb
bohdan_shevyrov <bohdan_shevyrov@epam.com>
2025-07-11 17:33:48 +0300

    feat: Add AWS S3 integration and configuration

---
+++ src/main/java/com/epam/edp/demo/config/AWSConfig.java
@@ -0,0 +1,20 @@
+package com.epam.edp.demo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class AWSConfig {
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+                .build();
+    }
+}
